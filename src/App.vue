9254d25d192b51e929f5079c2904cfa0a6ca97f7
xialiyisheng <xlys_000@126.com>
2020-06-25 06:24:58 +0800

    1-更改主页排版模式 2-加入element-ui模块

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,96 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <nav id="zypw-module" class="homepageGuide" v-if="true">
-        <router-link to="/homepage">homepage</router-link>
-        <router-link to="/pictures">photos</router-link>
-        <router-link to="/vlogs">v-log</router-link>
-        <router-link to="/profile">profile</router-link>
-        <router-view />
-      </nav>
-      <nav id="xlyspw-module" class="homepageGuide" v-else>
-        <router-link to="/tecs">tec</router-link>
-        <router-link to="/article">article</router-link>
-        <router-view />
-      </nav>
-    </div>
-
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-header style="text-align: center; font-size: 12px">
+        <div id="header">
+          <!--gutter属性来调整布局之间的宽度---分栏间隔,即水平方向的列距离-->
+          <el-row :gutter="0">
+            <el-col span="8">
+              <div class="header-col">
+                <el-avatar size="large" src="./assets/imgs/bgimg2.jpg"></el-avatar>
+              </div>
+            </el-col>
+            <el-col span="8">
+              <div class="header-col">
+                <el-row type="flex" justify="space-around">
+                  <div class="grid-content bg-purple-light">座右铭</div>
+                </el-row>
+                <el-row type="flex" justify="space-around">
+                  <div class="grid-content bg-purple-light">她那个时候年轻不懂事，不明白很多东西早已暗中标好了价格</div>
+                </el-row>
+              </div>
+            </el-col>
+            <el-col span="8">
+              <div class="header-col">
+                <el-row type="flex" class="row-bg" justify="space-around">
+                  <i class="el-icon-search">搜索</i>
+                </el-row>
+                <el-row type="flex" class="row-bg" justify="space-around">
+                  <i class="el-icon-setting">设置</i>
+                </el-row>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </el-header>
+      <!--<el-main>
+        <div id="body">
+          <el-row>
+            <el-col>
+              <div>
+                <router-link to="/homepage">homepage</router-link>
+              </div>
+            </el-col>
+            <el-col>
+              <div>
+                <router-link to="/pictures">photos</router-link>
+              </div>
+            </el-col>
+            <el-col>
+              <div>
+                <router-link to="/vlogs">v-log</router-link>
+              </div>
+            </el-col>
+            <el-col>
+              <div>
+                <router-link to="/profile">profile</router-link>
+              </div>
+            </el-col>
+            <router-view/>
+          </el-row>
+        </div>
+      </el-main>-->
+    </el-container>
   </div>
 </template>
 
 <style>
-#nav {
-  /*background-image: url("./assets/imgs/bgimg2.jpg");*/
-  background-size: cover; /*设置背景图充满屏幕*/
-  background-repeat: no-repeat; /*设置背景图不重复*/
-  position: fixed;
-  height: 100%; /*在Y轴上显示的部分占图片总体百分比*/
-  width: 100%; /*在X轴上显示的部分占图片总体百分比*/
-  top: 0px; /*设置与父级元素顶部的距离:也就是顶对齐*/
-  left: 0px; /*设置与父级元素左边的距离:也就是左对齐*/
-}
+  #header {
+    /*background-image: url("./assets/imgs/bgimg2.jpg");*/
+    background-size: cover; /*设置背景图充满屏幕*/
+    background-repeat: no-repeat; /*设置背景图不重复*/
+    position: fixed;
+    height: 100%; /*在Y轴上显示的部分占图片总体百分比*/
+    width: 100%; /*在X轴上显示的部分占图片总体百分比*/
+    top: 0px; /*设置与父级元素顶部的距离:也就是顶对齐*/
+    left: 0px; /*设置与父级元素左边的距离:也就是左对齐*/
+  }
 
-nav {
-  /*margin-left: 30%;*/
-  /*margin-right: 30%;*/
-  width: 100%;/*如果宽度不足以显示文本，则会自动向下扩展，即增加nav标签元素高度*/
-  height: 50px;
-  text-align: center;
-}
-.homepageGuide {
+  #body {
+    /*background-image: url("./assets/imgs/yasuo.jpg");*/
+    background-size: cover; /*设置背景图充满屏幕*/
+    background-repeat: no-repeat; /*设置背景图不重复*/
+    position: fixed;
+    height: 100%; /*在Y轴上显示的部分占图片总体百分比*/
+    width: 100%; /*在X轴上显示的部分占图片总体百分比*/
+    top: 0px; /*设置与父级元素顶部的距离:也就是顶对齐*/
+    left: 0px; /*设置与父级元素左边的距离:也就是左对齐*/
+  }
 
-}
+  .header-col{
+    height: 100%; /*在Y轴上显示的部分占图片总体百分比*/
+    width: 100%; /*在X轴上显示的部分占图片总体百分比*/
+  }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入element-ui和对应样式库 并使用  此处是全局引入，后续项目包体积太大可以考虑按需引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
