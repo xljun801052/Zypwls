cc9e9055233e645530dd4e35aa0a072010740b25
xialiyisheng <xlys_000@126.com>
2020-06-29 07:49:54 +0800

    1-自定义化图标使用展示个人信息 2-遗留问题：router-link重新渲染成一边待解决

--- src/App.vue
+++ src/App.vue
@@ -7,51 +7,35 @@
           <el-avatar :size="200" :src="bgimg"></el-avatar><!--./assets/imgs/bgimg2.jpg-->
         </div>
         <div class="header-col2">
-            <div class="grid-content bg-purple-light">Motto</div>
-            <div class="grid-content bg-purple-light">她那个时候年轻不懂事，不明白很多东西早已暗中标好了价格</div>
+          <div class="grid-content bg-purple-light">Motto</div>
+          <div class="grid-content bg-purple-light">她那个时候年轻不懂事，不明白很多东西早已暗中标好了价格</div>
         </div>
         <div class="header-col3">
-            <i id="qq" class="el-icon-setting">2074863066</i>
-            <i id="wechat" class="el-icon-setting">zy1614735214</i>
-            <i id="telephone" class="el-icon-setting">17317673034</i>
-            <i id="settings" class="el-icon-setting">设置</i>
+          <i id="qq" class="iconfont icon-zypw-QQ">2074863066</i>
+          <i id="wechat" class="iconfont icon-zypw-weixin">zy1614735214</i>
+          <i id="telephone" class="iconfont icon-zypw-iPhoneX">17317673034</i>
+          <i id="settings" class="el-icon-setting">设置</i>
         </div>
       </div>
       <div id="body">
         <div id="search">
-          <input id="searchInput" type="text" name="searchContent" placeholder="搜你所想..."></input>
-          <i id="searchIcon" class="el-icon-search"></i>
+          <el-input
+            id="searchInput"
+            style="width: 800px; height: 50px;margin-left: 650px;margin-top: 50px"
+            prefix-icon="iconfont icon-zypw-weixin"
+            suffix-icon="el-icon-search"
+            placeholder=" 搜你所想..."
+            v-model="input"
+            :disabled="false">
+          </el-input>
         </div>
-        <!--<el-row>
-          <el-col>
-            <div>
-              <router-link to="/homepage">homepage</router-link>
-            </div>
-          </el-col>
-          <el-col>
-            <div>
-              <router-link to="/pictures">photos</router-link>
-            </div>
-          </el-col>
-          <el-col>
-            <div>
-              <router-link to="/vlogs">v-log</router-link>
-            </div>
-          </el-col>
-          <el-col>
-            <div>
-              <router-link to="/profile">profile</router-link>
-            </div>
-          </el-col>
+        <div id="content">
+          <router-link to="/homepage">homepage</router-link>
+          <router-link to="/pictures">photos</router-link>
+          <router-link to="/vlogs">v-log</router-link>
+          <router-link to="/profile">profile</router-link>
           <router-view/>
-        </el-row>-->
-        <p>hahahha</p>
-        <p>hahahha</p>
-        <p>hahahha</p>
-        <p>hahahha</p>
-        <p>hahahha</p>
-        <p>hahahha</p>
-        <p>hahahha</p>
+        </div>
       </div>
     </div>
   </div>
@@ -59,11 +43,11 @@
 
 <script>
   export default {
-    name:'App',
-    data(){
-      return{
+    name: 'App',
+    data() {
+      return {
         //严重怀疑这种方式只能传网络资源图片
-        bgimg:'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+        bgimg: 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
       }
     }
   }
@@ -82,11 +66,10 @@
     display: flex;
     flex-direction: row;
     justify-content: space-around;
-    /*margin-bottom: 5px;*/
     /*background-image: url("./assets/imgs/bgimg2.jpg");*/
   }
 
-  .header-col2{
+  .header-col2 {
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
@@ -99,11 +82,11 @@
   .header-col3 {
     display: flex;
     flex-direction: column;
-    /*align-items: flex-start;*/
     justify-content: space-around;
-    font-size: 50px;
+    font-size: 30px;
   }
 
+  /*主体内容模块：上下分布*/
   #body {
     margin: 0px;
     /*background-image: url("./assets/imgs/bgpicture.jpg");*/
@@ -111,24 +94,19 @@
     background-repeat: no-repeat;
   }
 
-  #search{
+  /*content内容模块：上下分布*/
+  #content{
     display: flex;
     flex-direction: row;
-    justify-content: center;
+    justify-content:space-around ;
 
   }
 
-  #searchInput{
-      width: 1500px;
-      height: 50px;
-  }
   /*element-ui的icon设置大小是需要通过设置父盒子的font-size属性*/
-  #search{
+ /*#search {
     font-size: 50px;
     margin-top: 20px;
-  }
-
-
+  }*/
 
   /*#header {
     !*background-image: url("./assets/imgs/bgimg2.jpg");*!
@@ -142,22 +120,5 @@
 
     margin-bottom: 0px;
   }*/
-
-  /*#body {
-    !*background-image: url("./assets/imgs/yasuo.jpg");*!
-    background-size: cover; !*设置背景图充满屏幕*!
-    background-repeat: no-repeat; !*设置背景图不重复*!
-    position: fixed;
-    height: 100%; !*在Y轴上显示的部分占图片总体百分比*!
-    width: 100%; !*在X轴上显示的部分占图片总体百分比*!
-    top: 0px; !*设置与父级元素顶部的距离:也就是顶对齐*!
-    left: 0px; !*设置与父级元素左边的距离:也就是左对齐*!
-  }*/
-
-  /*.header-col {
-    height: 100%; !*在Y轴上显示的部分占图片总体百分比*!
-    width: 100%; !*在X轴上显示的部分占图片总体百分比*!
-  }*/
-
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 //引入element-ui和对应样式库 并使用  此处是全局引入，后续项目包体积太大可以考虑按需引入
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import  './assets/icons/icon-zypwls/iconfont.css'
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
